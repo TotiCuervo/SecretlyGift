3be2896781f62b39c4512b5a86f29fc4509ef4fa
Toti Cuervo <totifercuervo@gmail.com>
2023-10-25 23:16:08 -0400

    added navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,13 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+import localFont from 'next/font/local'
+
+const myFont = localFont({
+    src: './Baloo-Regular.ttf',
+    display: 'swap',
+    variable: '--font-baloo'
+})
 
 export const metadata: Metadata = {
     title: 'Create Next App',
@@ -11,7 +17,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang="en">
+        <html lang="en" className={`${myFont.variable}`}>
             <body>{children}</body>
         </html>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import Link from 'next/link'
 import Button from './components/button'
+import Navbar from './_components/nav-bar'
 
 function HomePage() {
     return (
-        <div className="h-screen w-screen bg-indigo-700">
-            <div className="container mx-auto p-12 pt-32 text-center text-white">
-                <h1 className="font-baloo text-9xl">Secretly</h1>
-                <p className="text-xl font-semibold">secret gift exchange made easy</p>
-            </div>
-            <div className="mx-auto text-center">
-                <Link href="/create-event">
-                    <Button>Get Started</Button>
-                </Link>
-            </div>
+        <div>
+            <Navbar />
+            {/* <div className="">
+                <div className="container mx-auto p-12 pt-32 text-center text-indigo-700">
+                    <h1 className="font-baloo text-9xl">Secretly</h1>
+                    <p className="text-xl font-semibold">secret gift exchange made easy</p>
+                </div>
+                <div className="mx-auto text-center">
+                    <Link href="/create-event">
+                        <Button>Get Started</Button>
+                    </Link>
+                </div>
+            </div> */}
         </div>
     )
 }
